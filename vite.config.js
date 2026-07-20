b945a1ce3691631818e43eca906296c9a37c0973
= <kaydeymyrtle@gmail.com>
2026-07-20 15:00:20 +0100

    Editing URL

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/': import.meta.env.BACKEND_URL || 'http://localhost:3000'
+      '/api/': process.env.VITE_BACKEND_URL || 'http://localhost:3000'
     }
   },
 })
